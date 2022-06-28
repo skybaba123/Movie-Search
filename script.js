859c1e2fb65f1194b4e1cf41b22cb23dc4111acc
Ugochukwu Miracle <77087947+skybaba123@users.noreply.github.com>
2022-06-28 15:42:17 +0100

    file uploaded

---
+++ script.js
@@ -0,0 +1,40 @@
+"strict";
+
+const movieContainer = document.querySelector(".movie-container");
+const search = document.querySelector(".search-input");
+
+const apiUrl = "http://www.omdbapi.com/?apikey=45f6b855";
+const api = () => {
+  fetch(`${apiUrl}&s=${search.value}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((result) => {
+      const data = result.Search;
+      console.log(data);
+
+      data.map((movie) => {
+        const html = `<div class="movie">
+      <img
+        src=${movie.Poster}
+        alt=""
+        class="movie-poster"
+      />
+      <h1>${movie.Title}</h1>
+      <p>${movie.Year}</p>
+    </div>`;
+        movieContainer.insertAdjacentHTML("afterbegin", html);
+      });
+    });
+};
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    movieContainer.innerHTML = "";
+    api();
+  }
+});
+
+// const newSearch = (searchs)=>{
+//  search.value = searchs
+// }
